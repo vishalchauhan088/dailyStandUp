e74798147de6def23919ef9044c031da4c1c2a22
vishalchauhan <vishal.chauhan@arrise.com>
2026-01-08 13:14:36 +0530

    initial commit

---
+++ src/test/java/com/vishalchauhan0688/dailyStandUp/DailyStandUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishalchauhan0688.dailyStandUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyStandUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
