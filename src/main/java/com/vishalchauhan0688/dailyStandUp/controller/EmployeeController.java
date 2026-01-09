8994d66fb72dc3573b881a5d1c20d411a22b742f
vishalchauhan <vishal.chauhan@arrise.com>
2026-01-09 13:32:11 +0530

    feat: add spring Basic security

--- src/main/java/com/vishalchauhan0688/dailyStandUp/controller/EmployeeController.java
+++ src/main/java/com/vishalchauhan0688/dailyStandUp/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
--- src/main/java/com/vishalchauhan0688/dailyStandUp/controller/HealthCheck.java
+++ src/main/java/com/vishalchauhan0688/dailyStandUp/controller/HealthCheck.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/health")
 public class HealthCheck {
 
     @GetMapping("/health")
